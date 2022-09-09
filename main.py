ff916fefb49137c1059d9f0dccfe49840115ec6b
yzjs99 <719146020@qq.com>
2022-09-09 14:11:27 +0800

    初始化三个文本区

--- main.py
+++ main.py
@@ -1,8 +1,21 @@
 import sys
-from PySide6.QtWidgets import QApplication, QLabel
+from PySide6.QtWidgets import QApplication, QLabel, QMessageBox
+from PySide6.QtCore import QFile
+from PySide6.QtGui import QIcon
+from PySide6.QtUiTools import QUiLoader
+
+class Window:
+
+    def __init__(self):
+
+        UiFile = QFile("UI.ui")
+        UiFile.open(QFile.ReadOnly)
+        UiFile.close()
+
+        self.ui = QUiLoader().load(UiFile)
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
-    label = QLabel("Hello World!")
-    label.show()
+    window = Window()
+    window.ui.show()
     app.exec()
