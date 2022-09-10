20e5b5bb69767cbb66a961dd1b9573b73ec987ec
yzjs99 <719146020@qq.com>
2022-09-10 17:53:33 +0800

    带行数的文本区，但是有bug

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 import sys
-from PySide6.QtWidgets import QApplication, QLabel, QMessageBox
+from PySide6.QtWidgets import QApplication, QLabel, QMessageBox, QGridLayout
 from PySide6.QtCore import QFile
 from PySide6.QtGui import QIcon
 from PySide6.QtUiTools import QUiLoader
+from QTextWithLineNum import QTextEditWithLineNum
 
 class Window:
 
@@ -13,6 +14,11 @@ class Window:
         UiFile.close()
 
         self.ui = QUiLoader().load(UiFile)
+
+        self.ui.qtCodeArea = QTextEditWithLineNum(self.ui.qtCodeArea)
+        self.ui.qtElementArea = QTextEditWithLineNum(self.ui.qtElementArea)
+        # self.ui.qtMatrixArea = QTextEditWithLineNum(self.ui.qtElementArea)
+
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
